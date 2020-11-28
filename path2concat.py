2ccc02b98d458fbd5d181aee86d750c0614c9270
ijulca <irene.julcac@gmail.com>
2020-11-28 11:05:09 +0800

    updated

--- path2concat.py
+++ path2concat.py
@@ -11,10 +11,41 @@ import sys, os
 sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py/')
 import genome_modules as GM
 
-def concat_genes(files, delimiter, pos):
+def load_seq_sp(files, delimiter, pos):
     seqs = {}
     for f in files:
-        
+        family = f.split('/')[-1].split('.')[0]
+        genes = {}
+        for line in open(f):
+            line = line.strip()
+            if '>' in line:
+                sp = line.split(delimiter)[pos]
+                if '>' in sp:
+                    sp = sp.replace('>','')
+                if sp not in genes:
+                    genes[sp] = []
+                else:
+                    print('ERROR...', 'duplicated genes for', sp)
+            else:
+                for b in line:
+                    genes[sp].append(b)
+        seqs[family] = genes
+    return seqs
+
+def get_species(seqs):
+    species = set([])
+    for s in seqs:
+        for n in seqs[s]:
+            species.add(n)
+    return species
+
+def concat_genes(files,delimiter,pos,outname):
+    seqs = load_seq_sp(files, delimiter, pos)
+    species = get_species(seqs)
+    print('')
+    
+    
+
     
 
 
