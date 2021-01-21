d23c54fdf9cdb455db49af9d5518f826dc1e1651
ijulca <irene.julcac@gmail.com>
2021-01-21 18:32:37 +0700

    updated

--- IQ3model2tree.py
+++ IQ3model2tree.py
@@ -6,8 +6,9 @@ Created on Tue Jan 19 18:48:24 2021
 @author: ijulca
 """
 import argparse, glob
-#import sys, os
-#sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py/')
+import sys, os
+sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py/')
+import general_modules as gmo
 
 
 iqtree = '/home/irene.julca/Programs/iqtree-2.1.2-Linux/bin/iqtree2'
@@ -30,18 +31,41 @@ def check_genetree(f):
             toprint = True
     return toprint
 
+def check_spider(f):
+    folder = f.split('model')[0]
+    log = folder+'/genetree.log'
+    files = glob.glob(folder+'/*')
+    toprint = False
+    if log in files:
+        with open(log, 'r') as f:
+            lines = f.read().splitlines()
+            last_line = lines[-1]
+            if 'Date and Time:' in last_line:
+                toprint = True
+        if toprint == True:
+            print(log)
+            #cmd = 'rm '+folder+'/genetree.*'
+            #gmo.run_command(cmd)
+    return toprint
+
 ### main
 parser = argparse.ArgumentParser(description="get the model and create the jobs for the tree reconstruction")
 parser.add_argument("-p", "--path", dest="path", default='./Data/', help="path where to create the folders")
+parser.add_argument("-s", "--spider", dest="spider", action='store_true', help="if activated will readd the genetree.log files and capture the unfinished jobs")
 args = parser.parse_args()
 
 path = args.path
+spider = args.spider
+print(spider)
 
 modelFiles = glob.glob(path+'/*/*/model.iqtree')
 g=0
 outfile = open('genetree.job', 'w')
 for f in modelFiles:
-    toprint = check_genetree(f)
+    if spider == True:
+        toprint = check_spider(f)
+    else:
+        toprint = check_genetree(f)
     if toprint == False:
         model = get_model(f)
         alg = f.split('model')[0]+f.split('/')[-2]+'.alg.clean'
