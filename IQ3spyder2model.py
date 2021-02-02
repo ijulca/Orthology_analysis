4eb3cb86e2f4ad5754e0850d240bf43fdeb9f05e
ijulca <irene.julcac@gmail.com>
2021-02-02 19:38:46 +0700

    updated

--- IQ3spyder2model.py
+++ IQ3spyder2model.py
@@ -11,8 +11,9 @@ sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py
 import general_modules as gmo
 
 iqtree = '/home/irene.julca/Programs/iqtree-2.1.2-Linux/bin/iqtree2'
+algtool = '/home/irene.julca/Programs/git_repository/phylome_analysis/salva_scripts/12.IndividualStep.Alignments.py'
 
-def check_spider(group,num_seq):
+def spider_model(group,num_seq):
     log = group+'/model.log'
     toprint = False
     if os.path.isfile(log) == True:
@@ -30,25 +31,52 @@ def check_spider(group,num_seq):
             gmo.run_command(cmd)
     return toprint, num_seq
 
+def spider_alg(path):
+    log = folder +'/'+folder.split('/')[-1]+'.log'
+    toprint = False
+    if os.path.isfile(log) == True:
+        with open(log, 'r') as f:
+            lines = f.read().splitlines()
+            if 'STEP	Multipple Sequence Alignment	END' in lines[-3]:
+                toprint = True
+    return toprint
+        
+    
 
-parser = argparse.ArgumentParser(description="get the model and create the jobs for the tree reconstruction")
+parser = argparse.ArgumentParser(description="spyder for aligment and model")
 parser.add_argument("-p", "--path", dest="path", default='./Data/', help="path where to create the folders")
+parser.add_argument("-t", "--tag", dest="tag", required=True, help="tag to get unfinished jobs for m=model, a=aligment")
+parser.add_argument("-c", "--config", dest="config", default = 'no',help="give for tag: a=aligment")
 args = parser.parse_args()
 
 path = args.path
+tag = args.tag
+configFile = args.config
+
 
 groups = glob.glob(path+'/*/*')
 
-outfile = open('model.job', 'w')
-num_seq = set([])
-for g in groups:
-    toprint, num_seq = check_spider(g, num_seq)
-    if toprint == False:
-        cmd = iqtree+' -s '+g+'/'+g.split('/')[-1]+'.alg.clean -m MF --prefix '+g+'/model -T 4'
-        print(cmd, file=outfile)
-outfile.close()
+if tag == 'm':
+    print('Spider for models...')
+    outfile = open('model.spyder.job', 'w')
+    num_seq = set([])
+    for g in groups:
+        toprint, num_seq = spider_model(g, num_seq)
+        if toprint == False:
+            cmd = iqtree+' -s '+g+'/'+g.split('/')[-1]+'.alg.clean -m MF --prefix '+g+'/model'#' -T 4'
+            print(cmd, file=outfile)
+    outfile.close()
 
-print('Unfinished jobs:', len(num_seq))
+    print('Unfinished jobs:', len(num_seq))
+elif tag == 'a':
+    print('Spider for aligment...')
+    outfile = open('alg.spyder.job', 'w')
+    for folder in groups:
+        toprint = spider_alg(folder)
+        if toprint == False:
+            cmd = algtool+' -c '+configFile+' -i '+folder +'/'+folder.split('/')[-1]+'.fa --cds '+folder +'/'+folder.split('/')[-1]+'.cds -p '+folder +'/'+folder.split('/')[-1]
+            print(cmd,file=outfile)
+    outfile.close()
 print('End')
     
     
---
+++ tree2branch.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 17:36:38 2021
+
+@author: ijulca
+"""
+import argparse, glob
+import sys,os
+sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py/')
+import phylome_analysis as PA
+import general_modules as gmo
+
+def check_spider(f, num_seq):
+    folder = f.split('model')[0]
+    log = folder+'/genetree.log'
+    toprint = False
+    if os.path.isfile(log) == True:
+        with open(log, 'r') as f:
+            lines = f.read().splitlines()
+            last_line = lines[-1]
+            if 'Date and Time:' in last_line:
+                toprint = True
+    return toprint
+
+
+def create_allTrees(treeFiles,outFile):
+    outfile = open(outFile, 'w')
+    for treeFile in treeFiles:
+        totree = check_spider(treeFile.replace('treefile','log'))
+        if totree == True:
+            group = treeFile.split('/')[-2]
+            tree = gmo.load_list(treeFile)
+            if len(tree) == 1:
+                print(group+'\t'+tree[0],file=outfile)
+            else:
+                print('ERROR...', treeFile)
+    outfile.close()
+
+def get_distFile(genetrees, outname):
+    outfile = open(outname, 'w')
+    for group in genetrees:
+        tree = genetrees[group]
+        dist = PA.get_average_branchLen(tree)
+        string = group +'\t'+str(dist)
+        print(string)
+    return outfile
+
+### main
+parser = argparse.ArgumentParser(description="get the gene trees and analysis")
+parser.add_argument("-p", "--path", dest="path", default='./Data/', help="path where is the data. Default=./Data/")
+#parser.add_argument("-s", "--spTree", dest="spTree", required=True, help="rooted species tree")
+args = parser.parse_args()
+
+path = args.path+'/'
+#spTreeFile = args.spTree
+
+alltreeFile = 'alltree.txt'
+ditFile = 'branch_lenght.txt'
+
+treeFiles = glob.glob(path +'*/*/genetree.treefile')
+
+
+if os.path.isfile(alltreeFile) == False:
+    create_allTrees(treeFiles,alltreeFile)
+
+### analysis
+genetrees = PA.get_trees_from_file(alltreeFile)
+print('Number of tree to be analysed:', len(genetrees))
+get_distFile(genetrees, ditFile)
+
+#spTree,spe2age = PA.load_species_tree(spTreeFile,'no')
+#print(spe2age)
+print('End...')
+    
+    
+        
+        
