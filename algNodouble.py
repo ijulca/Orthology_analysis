725c73f7c0b94a322e260da2b8911d94f008320a
ijulca <irene.julcac@gmail.com>
2021-02-17 12:21:41 +0800

    up

---
+++ algNodouble.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 12:08:41 2021
+
+@author: ijulca
+"""
+
+import argparse
+import sys, os
+import glob
+sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py/')
+import genome_modules as GM
+import random
+
+def get_doubles(data):
+    species = {}
+    for g in data:
+        sp = g.split('-')[-1]
+        if sp not in species:
+            species[sp]=set([])
+        species[sp].add(g)
+    seqs = []
+    for sp in species:
+        if len(species[sp]) !=1:
+            s = random.choice(species[sp])
+            seqs += [x for x in species[sp] if x != s]
+    return seqs
+            
+
+
+### main
+parser = argparse.ArgumentParser(description="remove duplicates from aligment")
+parser.add_argument("-p", "--path", dest="path", required=True, help="path to the aligned genes in fasta format")
+args = parser.parse_args()
+
+path = args.path
+
+files = glob.glob(path)
+
+for f in files:
+    seq = GM.load_sequences(f)
+    doubles = get_doubles(seq)
+    if len(doubles) != 0:
+        print(f)
+        outfile = open(f+'.2','w')
+        for s in seq:
+            if s not in doubles:
+                GM.print_sequence(s,''.join(seq[s]),outfile)
+        outfile.close()
+
+print('End...')
