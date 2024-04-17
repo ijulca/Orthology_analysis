c5f6a902bc382ef634a34cfbe5ebe488437bd7ae
ijulca <irene.jucac@gmail.com>
2024-04-17 14:06:31 +0200

    up

---
+++ oma2download.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 19:42:41 2024
+
+@author: ijulcach
+"""
+
+import argparse
+import sys,os
+sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/modules_py/')
+import general_modules as gmo
+import pyoma.browser.db
+from pyoma.browser.models import ProteinEntry
+db = pyoma.browser.db.Database('/work/FAC/FBM/DBC/cdessim2/oma/oma-browser/All.Jul2023/data/OmaServer.h5')
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+
+def hog2fasta(inFile, outpath):
+    gmo.create_folder(outpath)
+    hogs = gmo.load_list(inFile)
+    print('Number of HOGs:', len(hogs))
+    if len(hogs)>1000:
+        for i in range(0,len(hogs), 1000):
+            z = i + 1000
+            if z >len(hogs):
+                z = len(hogs)
+            outdir1 = outpath+str(i+1)+'-'+str(z)+'/'
+            gmo.create_folder(outdir1)
+            for j in range(i,z):
+                hog = hogs[j]
+                name = hog.split(':')[1]
+                outdir2 = outdir1+name+'/'
+                if os.path.isdir(outdir2) == False:
+                    gmo.create_folder(outdir2)
+                    outFile = outdir2+name+'.fasta'
+                    with open(outFile, 'wt') as fout:
+                        for p in db.member_of_hog_id(hog):
+                            pe = ProteinEntry(db, p)
+                            fout.write(f">{pe.omaid} {pe.canonicalid}\n")
+                            fout.write(pe.sequence)
+                            fout.write("\n\n")
+                else:
+                    print('Done for..', hog)
+    else:
+        for hog in hogs:
+            with open(f'hog_{hog}.fa', 'wt') as fout:
+                for p in db.member_of_hog_id(hog):
+                    pe = ProteinEntry(db, p)
+                    fout.write(f">{pe.omaid} {pe.canonicalid}\n")
+                    fout.write(pe.sequence)
+                    fout.write("\n\n")
+
+
+def mnemonic2fasta(inFile, outpath):
+    genomes = [pyoma.browser.models.Genome(db, g) for g in db.db.root.Genome.read()]
+    names = gmo.load_list(inFile)
+    spcode2taxaid = {}
+    for gen in genomes:
+        taxid  = gen.ncbi_taxon_id
+        name = gen.sciname
+        taxa = gen.uniprot_species_code
+        if taxa in names:
+            all_seq = list()
+            spcode2taxaid[taxa] = taxa +'\t'+taxid+'\t'+name
+            main_isos = [ProteinEntry(db, e) for e in db.main_isoforms(taxa)]
+            for iso in main_isos:
+                seq = iso.sequence
+                identifier = iso.canonicalid
+                description = iso.description
+                record = SeqRecord.SeqRecord(
+                Seq.Seq(seq),
+                id=identifier,
+                description=description)
+            all_seq.append(record)
+            with open(outpath+ taxa +  ".fa", "w") as output_handle:
+                SeqIO.write(all_seq, output_handle, "fasta")
+    
+    
+### main
+parser = argparse.ArgumentParser(description="download fasta file of hogs (root hogs)")
+parser.add_argument("-i", "--inFile", dest="inFile", required=True, help="list of hogs or list of mnemonic")
+parser.add_argument("-p", "--outpath", dest="outpath", required=True, help="folder where to create the files")
+parser.add_argument("-t", "--tag", dest="tag", required=True, help="what to download, h: hogs fasta, p: proteomes")
+args = parser.parse_args()
+
+inFile = args.inFile
+outpath = args.outpath
+
+if args.tag == 'h':
+    print('Downloading HOGS...')
+    hog2fasta(inFile, outpath)
+elif args.tag == 'p':
+    print('Downloading proteomes...')
+
+
+
--- tree2branch.py
+++ tree2branch.py
@@ -622,6 +622,7 @@ pathPlots = path + 'plots/'
 pathTables = path + 'tables/'
 famrateFile = pathTables+'fitted_family_info.tsv'
 speciesFile = '/home/ijulcach/projects/ldo_project/data/panther-18.0/species_tree.nhx'
+expect_branchFile = '/home/ijulcach/projects/ldo_project/results/expected_branches_pthr18.tsv.gz'
 
 pccFilePlant = path +'correlation/plant_specific.h5'
 pccFileAnimal = path +'correlation/animal_maincat.h5'
@@ -676,55 +677,14 @@ outtab5 = pathTables+'outgroup_structure.tsv'
 outtab6 = pathTables+'expression_samples.tsv'
 
 
-#######################
-#### PANTHER TREES #### 
-#######################
-### infiles and outfiles
-
-# treepath = '/home/ijulcach/projects/ldo_project/data/panther-18.0/trees/'
-# 
-# expPlantdata = '/home/ijulcach/projects/ldo_project/data/Plant_expression/Samples/'
-# taxaFile = '/home/ijulcach/projects/Land_Plants/nemo2taxa.txt'
-
-# inFile = pathTables+'pairwise_tests.tsv'
-# linFile = pathTables+'major_lineages_sp.txt'
-# expPath = '/home/ijulcach/projects/ldo_project/data/Plant_expression/Exp_matrices/'
-# # outfile1 = pathTables+'plant_exp_pcc_scaler.tsv'
-# # outfig1 = pathPlots+'plant_exp_pcc_scaler.svg'
-# # outfile1 = pathTables+'plant_exp_pcc_noscaler.tsv'
-# # outfig1 = pathPlots+'plant_exp_pcc_noscaler.svg'
-# # outfile1 = pathTables+'plant_exp_pcc_zscore.tsv' 
-# # outfig1 = pathPlots+'plant_exp_pcc_zscore.svg' 
-# # outfile1 = pathTables+'plant_exp_pcc_zscoremedian.tsv'
-# # outfig1 = pathPlots+'plant_exp_pcc_zscoremedian.svg'
-# # outfile1 = pathTables+'plant_exp_pcc_Snames.zscoreLog.tsv'
-# # outfig1 = pathPlots+'plant_exp_pcc_Snames.zscoreLog.svg'
-# outfile1 = pathTables+'plant_exp_pcc_zscoreLog.tsv'  ### I use this
-# outfig1 = pathPlots+'plant_exp_pcc_zscoreLog.svg'    ### I use this
-# outfig2 = pathPlots+'plant_exp_pcc_zscoreLog_nodesS.svg'
-# outfig3 = pathPlots+'plant_exp_pcc_zscoreLog_nodesI.svg'
-# outfig4 = pathPlots +'plant_exp_pcc_zscoreLog_sp.svg'
-# outfig5 = pathPlots +'plant_exp_pcc_zscoreLog_spS.svg'
-# outfig6 = pathPlots +'plant_exp_pcc_zscoreLog_spI.svg'
-
-# files = glob.glob('/home/ijulcach/projects/ldo_project/data/Plant_expression/Exp_matrices/*/*.Gnames.sample.txt')
-# outname1 = pathTables+'plant_sampleSPM.tsv'
-# outname2 = pathTables + 'plant_sampleSPM_matrix.tsv'
-# outfig7 = pathPlots+'plant_samplesSPM.svg'
-# outfig8 = pathPlots+'plant_samplesSPMS.svg'
-# outfig9 = pathPlots+'plant_samplesSPMI.svg'
-# outfig10 = pathPlots+'plant_samplesSPM_sp.svg'
-# outfig11 = pathPlots+'plant_samplesSPM_spS.svg'
-# outfig12 = pathPlots+'plant_samplesSPM_spI.svg'
-# outfig13 = pathPlots+'supplementary_spm_plants.svg'
-# outsampTable = pathTables+'sp2number_samples.tsv'
-# outfigs = pathPlots+'sp2number_samples.svg'
-
-
 ##############################
 #### Duplication analysis ####
 ##############################
-categories = ['normal-normal', 'short-short', 'long-long', 'normal-long','normal-short', 'short-long']
+
+df = pd.read_csv(expect_branchFile, sep='\t')
+print(df.columns.values)
+
+# categories = ['normal-normal', 'short-short', 'long-long', 'normal-long','normal-short', 'short-long']
 
 ##### family-specific rate ### Supplementary Figure 1
 # df = pd.read_csv(famrateFile, sep='\t', header=0)
@@ -840,117 +800,118 @@ categories = ['normal-normal', 'short-short', 'long-long', 'normal-long','normal
 # outfile.close()
 
 ###### Plot of the categories ## Figure 2
-lineages = ['Protostomia', 'Deuterostomia','TRIAD','NEMVE','MONBE', 'Fungi', 'Amoebozoa', 'Excavates',
-            'Viridiplantae','Alveolata-Stramenopiles','Archaea', 'Eubacteria']
-lineages.reverse()
-df = pd.read_csv(node2catLinFile, sep='\t', header=0)
-df['total'] = df.sum(numeric_only=True, axis=1)
-df['totalper']=df['total']*100/df['total'].sum()
-df.to_csv(path_or_buf=node2catLin_perFile, sep='\t', header=True, index=False)
-df = df[df['Taxa'].isin(lineages) == True] ## remove the ones in internal nodes
-df.sort_values(by=['Taxa'], key=lambda column: column.map(lambda e: lineages.index(e)), inplace=True)
+# lineages = ['Protostomia', 'Deuterostomia','TRIAD','NEMVE','MONBE', 'Fungi', 'Amoebozoa', 'Excavates',
+#             'Viridiplantae','Alveolata-Stramenopiles','Archaea', 'Eubacteria']
+# lineages.reverse()
+# df = pd.read_csv(node2catLinFile, sep='\t', header=0)
+# df['total'] = df.sum(numeric_only=True, axis=1)
+# df['totalper']=df['total']*100/df['total'].sum()
+# df.to_csv(path_or_buf=node2catLin_perFile, sep='\t', header=True, index=False)
+# df = df[df['Taxa'].isin(lineages) == True] ## remove the ones in internal nodes
+# df.sort_values(by=['Taxa'], key=lambda column: column.map(lambda e: lineages.index(e)), inplace=True)
 
-df2 = pd.DataFrame() ## dataframe of categories
-for e in categories:
-    df2[e] = df[e]*100/df['total']
-df2['Taxa'] = df['Taxa']
-df2.to_csv(path_or_buf=node2catLin_perCatFile, sep='\t', header=True, index=False)
+# df2 = pd.DataFrame() ## dataframe of categories
+# for e in categories:
+#     df2[e] = df[e]*100/df['total']
+# df2['Taxa'] = df['Taxa']
+# df2.to_csv(path_or_buf=node2catLin_perCatFile, sep='\t', header=True, index=False)
 
-palette = ['#3182bdff','#9ecae1ff','#deebf7ff','#e6550dff','#fdae6bff','#fee6cec4']
-bar_width = 0.4
-bar_positions = range(len(df))
-fig, ax = plt.subplots()
-ax = df2.plot(x='Taxa', y=categories, kind='barh', stacked=True, color=palette, alpha=.7, ax=ax,
-              figsize=(6, 7.5), width=bar_width)
+# palette = ['#3182bdff','#9ecae1ff','#deebf7ff','#e6550dff','#fdae6bff','#fee6cec4']
+# bar_width = 0.4
+# bar_positions = range(len(df))
+# fig, ax = plt.subplots()
+# ax = df2.plot(x='Taxa', y=categories, kind='barh', stacked=True, color=palette, alpha=.7, ax=ax,
+#               figsize=(6, 7.5), width=bar_width)
 
-ax.barh([p + bar_width for p in bar_positions],df['totalper'], bar_width, color='#808080ff', label='duplications')
-plt.xticks(fontsize=23)
-plt.savefig(dupFigure2, bbox_inches='tight')
-plt.show()
+# ax.barh([p + bar_width for p in bar_positions],df['totalper'], bar_width, color='#808080ff', label='duplications')
+# plt.xticks(fontsize=23)
+# plt.savefig(dupFigure2, bbox_inches='tight')
+# plt.show()
 
 
-##### Plot of the categories per lineage per node, Figure 3
-## Create the file
-species = set()
-lineages = {}
-for line in open(majorLinFile):
-    line = line.strip()
-    data = line.split('\t')
-    lineages[data[0]] = data[2].split('; ')
-    for e in data[2].split('; '):
-        if len(e) <=5 and e.isupper():
-            species.add(e)
-print('Number of species', len(species))
+# ##### Plot of the categories per lineage per node, Figure 3
+# ## Create the file
+# species = set()
+# lineages = {}
+# for line in open(majorLinFile):
+#     line = line.strip()
+#     data = line.split('\t')
+#     lineages[data[0]] = data[2].split('; ')
+#     for e in data[2].split('; '):
+#         if len(e) <=5 and e.isupper():
+#             species.add(e)
+# print('Number of species', len(species))
 
-table = {}
-for line in open(node2catFile):
-    line = line.strip()
-    data = line.split('\t')
-    if data[0] != 'Taxa':
-        table[data[0]] = [float(x) for x in data[1:]]
-ranks = {}
-for l in lineages:
-    ranks[l] = {}
-    ranks[l]['Internal'] = table[l]
-    for tax in lineages[l]:
-        if tax in species:
-            key = 'Terminal'#+l[0]
-        else:
-            key = 'Internal'#+l[0] ### tax
-        if key not in ranks[l]:
-            ranks[l][key]=[0.0]*len(categories)
-        if tax in table:
-            nums = table[tax]
-            ranks[l][key] = [x + y for x, y in zip(ranks[l][key], nums)]
-outfile = open(nodesRanksFile,'w')
-head = 'Major_Rank\tRank\t'+'\t'.join(categories)
-print(head, file=outfile)
-for l in ranks:
-    for e in ranks[l]:
-        string = l+'\t'+e+'\t'+'\t'.join([str(x) for x in ranks[l][e]])
-        print(string,file=outfile)
-outfile.close()
+# table = {}
+# for line in open(node2catFile):
+#     line = line.strip()
+#     data = line.split('\t')
+#     if data[0] != 'Taxa':
+#         table[data[0]] = [float(x) for x in data[1:]]
+# ranks = {}
+# for l in lineages:
+#     ranks[l] = {}
+#     ranks[l]['Internal'] = table[l]
+#     for tax in lineages[l]:
+#         if tax in species:
+#             key = 'Terminal'#+l[0]
+#         else:
+#             key = 'Internal'#+l[0] ### tax
+#         if key not in ranks[l]:
+#             ranks[l][key]=[0.0]*len(categories)
+#         if tax in table:
+#             nums = table[tax]
+#             ranks[l][key] = [x + y for x, y in zip(ranks[l][key], nums)]
+# outfile = open(nodesRanksFile,'w')
+# head = 'Major_Rank\tRank\t'+'\t'.join(categories)
+# print(head, file=outfile)
+# for l in ranks:
+#     for e in ranks[l]:
+#         string = l+'\t'+e+'\t'+'\t'.join([str(x) for x in ranks[l][e]])
+#         print(string,file=outfile)
+# outfile.close()
 
-#### Create the plot Figure 3
+# #### Create the plot Figure 3
 
-lineages = ['Protostomia', 'Deuterostomia', 'Fungi', 'Amoebozoa', 'Excavates', ### we removed 'TRIAD', 'NEMVE','MONBE'
-            'Viridiplantae','Alveolata-Stramenopiles','Archaea', 'Eubacteria']
-lineages.reverse()
-df = pd.read_csv(nodesRanksFile, sep='\t', header=0) 
-df = df[df['Major_Rank'].isin(lineages) == True] ### we removed 'TRIAD', 'NEMVE','MONBE'
+# lineages = ['Protostomia', 'Deuterostomia', 'Fungi', 'Amoebozoa', 'Excavates', ### we removed 'TRIAD', 'NEMVE','MONBE'
+#             'Viridiplantae','Alveolata-Stramenopiles','Archaea', 'Eubacteria']
+# lineages.reverse()
+# df = pd.read_csv(nodesRanksFile, sep='\t', header=0) 
+# df = df[df['Major_Rank'].isin(lineages) == True] ### we removed 'TRIAD', 'NEMVE','MONBE'
 
-## calculate the total percentage of duplications
-for index, row in df.iterrows():
-    taxa = df['Major_Rank'].values[index]
-    df2 = df[df['Major_Rank'] == taxa]
-    total = df2[categories].sum()  # getting the total number of duplications per lineage
-    num = row.values[2:]
-    df.at[index, 'TotalPer'] = np.sum(num)*100/total.sum()
-    sym = (row[['normal-normal','short-short','long-long']].sum())*100/np.sum(num)
-    asym = (row[['normal-long','normal-short','short-long']].sum())*100/np.sum(num)
-    df.at[index,'symmetric'] = sym
-    df.at[index,'asymmetric'] = asym + sym
+# ## calculate the total percentage of duplications
+# for index, row in df.iterrows():
+#     taxa = df['Major_Rank'].values[index]
+#     df2 = df[df['Major_Rank'] == taxa]
+#     total = df2[categories].sum()  # getting the total number of duplications per lineage
+#     num = row.values[2:]
+#     df.at[index, 'TotalPer'] = np.sum(num)*100/total.sum()
+#     sym = (row[['normal-normal','short-short','long-long']].sum())*100/np.sum(num)
+#     asym = (row[['normal-long','normal-short','short-long']].sum())*100/np.sum(num)
+#     df.at[index,'symmetric'] = sym
+#     df.at[index,'asymmetric'] = asym + sym
 
-palettedup = ['#bdbdbd','#636363']
-palettecata = ['#fdae6bff','#e6550dff']  #d95f0e
-palettecats = ['#9ecae1ff','#3182bdff']  
-fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True, gridspec_kw={'height_ratios': [1, 4]})#, gridspec_kw={'height_ratios': [1, 1, 3, 3]})#, figsize=(16,14), sharey=True) ##Fig1: Plants: 4,4 figsize=(15, 10), 3,4
-axes = axes.flatten()
-sns.barplot(df, x="TotalPer", y="Major_Rank", hue="Rank", palette=palettedup, ax=axes[2], alpha=.7)
-sns.barplot(df, x='asymmetric', y='Major_Rank', hue='Rank', palette=palettecata, legend=False, ax=axes[3], alpha=.7)
-sns.barplot(df, x='symmetric', y='Major_Rank', hue='Rank', palette=['white','white'], legend=False, ax=axes[3], alpha=.7)
-sns.barplot(df, x='symmetric', y='Major_Rank', hue='Rank', palette=palettecats, legend=False, ax=axes[3], alpha=.7)
+# palettedup = ['#bdbdbd','#636363']
+# palettecata = ['#fdae6bff','#e6550dff']  #d95f0e
+# palettecats = ['#9ecae1ff','#3182bdff']  
+# fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True, gridspec_kw={'height_ratios': [1, 4]})#, gridspec_kw={'height_ratios': [1, 1, 3, 3]})#, figsize=(16,14), sharey=True) ##Fig1: Plants: 4,4 figsize=(15, 10), 3,4
+# axes = axes.flatten()
+# sns.barplot(df, x="TotalPer", y="Major_Rank", hue="Rank", palette=palettedup, ax=axes[2], alpha=.7)
+# sns.barplot(df, x='asymmetric', y='Major_Rank', hue='Rank', palette=palettecata, legend=False, ax=axes[3], alpha=.7)
+# sns.barplot(df, x='symmetric', y='Major_Rank', hue='Rank', palette=['white','white'], legend=False, ax=axes[3], alpha=.7)
+# sns.barplot(df, x='symmetric', y='Major_Rank', hue='Rank', palette=palettecats, legend=False, ax=axes[3], alpha=.7)
 
-dfper = df[['Rank','TotalPer']].groupby(by=['Rank']).mean().T
-dfper = dfper[['Terminal','Internal']]
-dfper.plot(kind='barh', color=palettedup, ax=axes[0], alpha=.7)
-dfcat = df[['Rank','symmetric','asymmetric']].groupby(by=['Rank']).mean()
-dfcat = dfcat.reindex(['Terminal','Internal'])
-dfcat['asymmetric'] = 100 - dfcat['symmetric']
-dfcat.plot(kind='barh', color=['#3182bdff','#e6550dff'], stacked=True, ax=axes[1], alpha=.7)
-plt.savefig(dupFigure3, bbox_inches='tight') ##Supplementary Fig 3  
+# dfper = df[['Rank','TotalPer']].groupby(by=['Rank']).mean().T
+# dfper = dfper[['Terminal','Internal']]
+# dfper.plot(kind='barh', color=palettedup, ax=axes[0], alpha=.7)
+# axes[0].set_xticklabels(['All lineages'] )
+# dfcat = df[['Rank','symmetric','asymmetric']].groupby(by=['Rank']).mean()
+# dfcat = dfcat.reindex(['Terminal','Internal'])
+# dfcat['asymmetric'] = 100 - dfcat['symmetric']
+# dfcat.plot(kind='barh', color=['#3182bdff','#e6550dff'], stacked=True, ax=axes[1], alpha=.7)
+# plt.savefig(dupFigure3, bbox_inches='tight') ##Supplementary Fig 3  
 
-plt.show()
+# plt.show()
     
 
 
