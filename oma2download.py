260f0448adf292d5aedacf6613eb7d957555fec8
ijulca <irene.jucac@gmail.com>
2024-10-02 14:51:36 +0200

    up

--- oma2download.py
+++ oma2download.py
@@ -150,7 +150,7 @@ def pep2hogID(inFile):
     db_search = db.id_mapper['XRef']
     outfile = open(f"{inFile}.hogs",'w')
     for g in genes:
-        info = [x[1]['EntryNr'] for x in db_search(g)]
+        info = [x[1]['EntryNr'] for x in db_search.search_xref(g)]
         hogs = set([db.hog_family(x) for x in info])
         for hog in hogs:
             print(f"{g}\t{hog}", file=outfile)
